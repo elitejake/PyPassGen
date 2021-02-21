1ec4f033c02943491a155cfb1a9f3b3b1c7650c5
elitejake <74049394+elitejake@users.noreply.github.com>
2021-02-21 19:50:55 +0530

    fixed ImportError

--- defaults.py
+++ defaults.py
@@ -3,6 +3,8 @@
 @author: elitejake
 """
 
+import sys
+
 defaults = 'Configuration for PyPassGen\n\nLength of password:\n16\n\nNumber of passwords to generate:\n10\n\nOutput filename: (optional)\npasswords.txt\n\nPrint generated password(s) in the terminal?: [Y, N]\nN\n\nUse secrets module?: [Y, N]\nY\n\n\nCan the password contain...\n\n... upper-case alphabets: [Y, N]\nY\n\n... numbers: [Y, N]\nY\n\n... symbols: [Y, N]\nY\n\nIf you somehow mess this file up, use defaults.py to reset this all the parameters.'
 
 try:
