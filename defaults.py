7eb0f9cdb621278b8837d6fbc79c19dfbcad6a15
elitejake <74049394+elitejake@users.noreply.github.com>
2021-02-27 15:55:40 +0530

    Update defaults.py

--- defaults.py
+++ defaults.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 @author: elitejake
 """
-
 import sys
 
-defaults = 'Configuration for PyPassGen\n\nLength of password:\n16\n\nNumber of passwords to generate:\n10\n\nOutput filename: (optional)\npasswords.txt\n\nPrint generated password(s) in the terminal?: [Y, N]\nN\n\nUse secrets module?: [Y, N]\nY\n\n\nCan the password contain...\n\n... upper-case alphabets: [Y, N]\nY\n\n... numbers: [Y, N]\nY\n\n... symbols: [Y, N]\nY\n\nIf you somehow mess this file up, use defaults.py to reset this all the parameters.'
+defaults = 'Configuration for PyPassGen\n\nLength of password:\n16\n\nNumber of passwords to generate:\n10\n\nOutput filename: (optional)\npasswords.txt\n\nPrint generated password(s) in the terminal?: [Y, N]\nN\n\nUse secrets module?: [Y, N]\nY\n\n\nCan the password contain...\n\n... upper-case alphabets: [Y, N]\nY\n\n... numbers: [Y, N]\nY\n\n... symbols: [Y, N]\nY\n\nIf you somehow mess this file up, use defaults.py to reset this all the parameters.\nUse \'PyPassGen-li.py -C\' to generate password using this configuration.'
 
 try:
     with open('config.txt', 'w') as config:
